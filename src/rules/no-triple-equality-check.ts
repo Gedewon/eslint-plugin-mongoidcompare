e47aad392be6d44806bda237b4504ff64e285f97
gedewon <gedewon.hayle.9@gmail.com>
2022-12-18 17:20:32 +0300

    Create a top level symbol comparison to check ObjectID types

--- src/rules/no-triple-equality-check.ts
+++ src/rules/no-triple-equality-check.ts
@@ -1,17 +1,31 @@
 import { ESLintUtils, TSESLint, TSESTree } from '@typescript-eslint/utils';
 import { RuleModule } from '@typescript-eslint/utils/dist/ts-eslint';
+import * as tsutils from 'tsutils';
 import * as ts from 'typescript';
 
 const createRule = ESLintUtils.RuleCreator(
   name => `https://github.com/Gedewon/eslint-plugin-mongoidcompare/blob/main/doc.md`,
 );
 
-const isBothSideMongoIdTypes = (leftNodeType:ts.Type,rightNodeType:ts.Type,checker:ts.TypeChecker):boolean=>{ 
-  return (checker.typeToString(leftNodeType).search(`ObjectId`) !==-1
-       && checker.typeToString(rightNodeType).search(`ObjectId`) !== -1
-  );
+export function isObjectID(type: ts.Type, tc: ts.TypeChecker): boolean {
+  if (tsutils.isTypeReference(type)) {
+    type = type.target;
+  }
+  if (type.symbol !== undefined && type.symbol.name === 'ObjectId') {
+    return true;
+  }
+  // handle union types recursively
+  if (tsutils.isUnionOrIntersectionType(type)) {
+    return type.types.some(t => isObjectID(t, tc));
+  }
+  const bases = type.getBaseTypes();
+  return bases !== undefined && bases.some(t => isObjectID(t, tc));
 }
 
+
+const isBothSideMongoIdTypes = (leftNodeType:ts.Type,rightNodeType:ts.Type,checker:ts.TypeChecker):boolean=>{
+  return (isObjectID(leftNodeType,checker) && isObjectID(rightNodeType,checker));
+}
 
 const rule:TSESLint.RuleModule<"noTripleEqualityCheck" , never[], {
   BinaryExpression(node: TSESTree.BinaryExpression): void;
