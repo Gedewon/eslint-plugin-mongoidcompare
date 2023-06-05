eee5d88d9e041cc78b2558cc007b5feca7268555
Jin <>
2023-06-06 02:37:39 +0800

    Fix the linter bug - Only check the triple equality operator - Fix the typescript compilation error by using latest packages - Add test case for `instanceof` operator - Add tsconfig for test - Remove unnecessary files - Add comment into `file.ts` - Add generated `file.*.*` to .gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import noTripleEquality from "./rules/no-triple-equality-check";
+import noTripleEquality from './rules/no-triple-equality-check'
 
-export = { 
-    rules: { 
-        "no-triple-equality-check":noTripleEquality
-    },
-};
+export = {
+  rules: {
+    'no-triple-equality-check': noTripleEquality,
+  },
+}
